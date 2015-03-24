d88275029be1d9940f5411cc395d4f6e583971ea
Eric <naibau2004@gamil.com>
2015-03-25 02:30:37 +0800

    Commit 2015/03/25

--- src/CronMail/CronMail.java
+++ src/CronMail/CronMail.java
@@ -3,8 +3,6 @@ package CronMail;
 public class CronMail {
 	
 	public static void main ( String [] args ){
-		
-		StartCMD sc = new StartCMD () ;
-		sc.start(args[0], args[1]);
+
 	}
 }
--- src/CronMail/StartCMD.java
+++ src/CronMail/StartCMD.java
@@ -1,52 +1,102 @@
 package CronMail;
 
-import java.util.*;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class StartCMD {
-	private int returnCode ;
 
-	public int start (String cmdLine , String errorLogPath) {
+	private String cmdPath ;
+	private String errLogPath ;
+	
+	String getCmdPath() {
+		return cmdPath;
+	}
+
+	String getErrLogPath() {
+		return errLogPath;
+	}
+
+	public StartCMD (String cmdPath , String errLogPath){
+		this.cmdPath = cmdPath ;
+		this.errLogPath = errLogPath ;
+	}
+	
+	private int checkErrLogStatus (String errLogPath){
+		int logStatus = 0 ;
+		File logFile = new File (errLogPath) ;
 		
+		if (logFile.exists() == false){
+			try {
+				logFile.createNewFile();
+			} catch (IOException e) {
+				return -1 ;
+			}
+		}	
+		
+		if ( logFile.canRead() == false){
+			return logStatus = -2 ;
+		}
+		
+		if ( logFile.canWrite() == false){
+			return logStatus = -3 ;
+		}
+		return 0 ;
+	}
+	
+	public int linux (String cmdPath){
+		int logStatus = checkErrLogStatus (getErrLogPath()) ;
 		String [] cmdArray = new String[3] ;
 		cmdArray[0] = ("/bin/sh");
     	cmdArray[1] = ("-c");
-    	//cmdArray[2] = (cmdLine + " 2> " + errorLogPath) ;
-    	cmdArray[2] = (cmdLine) ;
-		Path p1 = Paths.get(errorLogPath);
-		boolean checkFile = Files.exists(p1) ;
+    	cmdArray[2] = (cmdPath) ;
+    	
+    	if (logStatus != 0){
+    		return -1 ;
+    	}
+    	
 		
-		if ( checkFile == false ){
-			System.out.println ("log file can not access") ;
-			return -100 ;
-		}
-    	Process proc = null ;
-    	try{
-    		proc = Runtime.getRuntime().exec(cmdArray);
-			InputStream stderr = proc.getErrorStream();
-			InputStreamReader isr = new InputStreamReader(stderr);
-			BufferedReader br = new BufferedReader(isr);
-			
-			String line = null ;	
-			while ((line = br.readLine()) != null){
-				String next = line + "\n" ;
-				Files.write (p1 , line.getBytes(), StandardOpenOption.APPEND);
-			}
-			
-			File logFile = new File (errorLogPath) ;
-        	if ( logFile.length() != 0){
-        		return -11 ;
-        	}else{
-        		return 0 ;
-        	}
-				
-    	}catch (Exception e){
+		return 0;
+	}
+	
+	
+	
+//	public int start (String cmdLine , String errorLogPath) {
+//		
+//		String [] cmdArray = new String[3] ;
+//		cmdArray[0] = ("/bin/sh");
+//    	cmdArray[1] = ("-c");s
+//    	cmdArray[2] = (cmdLine) ;
+//		Path p1 = Paths.get(errorLogPath);
+//		boolean checkFile = Files.exists(p1) ;
+//		
+//		if ( checkFile == false ){
+//			System.out.println ("log file can not access") ;
+//			return -100 ;
+//		}
+//    	Process proc = null ;
+//    	try{
+//    		proc = Runtime.getRuntime().exec(cmdArray);
+//			InputStream stderr = proc.getErrorStream();
+//			InputStreamReader isr = new InputStreamReader(stderr);
+//			BufferedReader br = new BufferedReader(isr);
+//			
+//			String line = null ;	
+//			while ((line = br.readLine()) != null){
+//				String next = line + "\n" ;
+//				Files.write (p1 , line.getBytes(), StandardOpenOption.APPEND);
+//			}
+//			
+//			File logFile = new File (errorLogPath) ;
+//        	if ( logFile.length() != 0){
+//        		return -11 ;
+//        	}else{
+//        		return 0 ;
+//        	}
+//				
+//    	}catch (Exception e){
 //    		try {
 //				proc.getInputStream().close();
 //				proc.getOutputStream().close();
@@ -55,15 +105,15 @@ public class StartCMD {
 //				// TODO Auto-generated catch block
 //				e2.printStackTrace();
 //			}
-    		  		
-    		String except = e.getLocalizedMessage() + "\n" ;
-			try {
-				Files.write (p1 , except.getBytes(), StandardOpenOption.APPEND);
-				return -12 ;
-			} catch (IOException e1) {
-				e1.printStackTrace();
-				return -101 ;
-			}
-    	}
-	}
+//    		  		
+//    		String except = e.getLocalizedMessage() + "\n" ;
+//			try {
+//				Files.write (p1 , except.getBytes(), StandardOpenOption.APPEND);
+//				return -12 ;
+//			} catch (IOException e1) {
+//				e1.printStackTrace();
+//				return -101 ;
+//			}
+//    	}
+//	}
 }
